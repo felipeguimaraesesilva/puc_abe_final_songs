e645edf1ee7722e8f0505b9e6b4b1ffac7688c73
felipeguimaraesesilva <felipeguimaraesesilva@gmail.com>
2019-06-09 14:53:51 -0300

    Versionamento

--- puc_abe_songs_api/src/main/java/com/guimaraes/controller/SongController.java
+++ puc_abe_songs_api/src/main/java/com/guimaraes/controller/SongController.java
@@ -6,12 +6,16 @@ import java.util.List;
 import org.jmusixmatch.MusixMatch;
 import org.jmusixmatch.MusixMatchException;
 import org.jmusixmatch.entity.track.Track;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.guimaraes.constant.AppConstants;
+import com.netflix.discovery.EurekaClient;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @EnableCircuitBreaker
@@ -20,6 +24,13 @@ public class SongController {
 
 	private static final String RESOURCE_NAME = "/song";
 	private static final String CONTEXT_RESOURCE = "/" + AppConstants.VERSION_V1 + RESOURCE_NAME;
+
+	@Autowired
+	@Lazy
+	private EurekaClient eurekaClient;
+
+	@Value("${spring.application.name}")
+	private String appName;
 
 	@HystrixCommand(fallbackMethod = "reliable")
 	@GetMapping(CONTEXT_RESOURCE)
