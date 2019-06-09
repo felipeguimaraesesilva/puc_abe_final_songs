87b16e68fd1f57bbbe3422dc9132d272dcd63237
felipeguimaraesesilva <felipeguimaraesesilva@gmail.com>
2019-06-09 12:36:00 -0300

    Versionamento

--- puc_abe_songs_api/src/main/java/com/guimaraes/controller/SongController.java
+++ puc_abe_songs_api/src/main/java/com/guimaraes/controller/SongController.java
@@ -6,18 +6,22 @@ import java.util.List;
 import org.jmusixmatch.MusixMatch;
 import org.jmusixmatch.MusixMatchException;
 import org.jmusixmatch.entity.track.Track;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.guimaraes.constant.AppConstants;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
+@EnableCircuitBreaker
 @RestController
 public class SongController {
 
 	private static final String RESOURCE_NAME = "/song";
 	private static final String CONTEXT_RESOURCE = "/" + AppConstants.VERSION_V1 + RESOURCE_NAME;
 
+	@HystrixCommand(fallbackMethod = "reliable")
 	@GetMapping(CONTEXT_RESOURCE)
 	public List<Track> getSongByFilters(@RequestParam("artistName") String artistName,
 			@RequestParam("songName") String songName) {
@@ -29,6 +33,10 @@ public class SongController {
 			e.printStackTrace();
 		}
 
+		return new ArrayList<>();
+	}
+
+	public List<Track> reliable(String artistName, String songName) {
 		return new ArrayList<>();
 	}
 
